bd8ede8bf403da17114cbaad522e47fada9bf844
yuvwwa <yuvwwa@gmail.com>
2026-03-03 14:32:01 +0800

    machine part1 yulya and masha

---
+++ sketch_mar3a/sketch_mar3a.ino
@@ -0,0 +1,31 @@
+#define DIR_1 4
+#define SPEED_1 5
+
+#define DIR_2 7
+#define SPEED_2 6
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(DIR_1, OUTPUT);
+  pinMode(DIR_2, OUTPUT);
+  pinMode(SPEED_1, OUTPUT);
+  pinMode(SPEED_2, OUTPUT);
+
+  digitalWrite(DIR_1, HIGH);
+  digitalWrite(DIR_2, LOW);
+  digitalWrite(SPEED_1, HIGH);
+  digitalWrite(SPEED_2, LOW);
+
+  delay(2000);
+
+  digitalWrite(DIR_1, LOW);
+  digitalWrite(DIR_2, LOW);
+  digitalWrite(SPEED_1, HIGH);
+  digitalWrite(SPEED_2, LOW);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
